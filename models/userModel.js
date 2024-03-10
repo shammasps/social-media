f9aca0fe8b87131830f2bdce647b0abe13caaaa9
shammas <shammasps417@gmail.com>
2024-03-10 15:50:22 +0530

    create-post-design

--- models/userModel.js
+++ models/userModel.js
@@ -1,27 +1,36 @@
-// itemModel.js
-
 const mongoose = require('../config/db');
+const data = require('../models/data');
 
-const User = mongoose.model('User', 
-{ 
-    firstname: String ,
-    lastname: String ,
-    username: String ,
-    email: String ,
-    password: String ,
-    gender: String ,
-    profilePicture: String,
-    phone: String,
-    birthday:String,
-    aboutMe:String,
-    followerList: [{
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-        followedOn: { type: Date, default: Date.now }
-      }],
-      skills: [{ type: String }] // Define the skills array field
-    
+const userSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  username: String,
+  email: String,
+  password: String,
+  gender: String,
+  profilePicture: String,
+  phone: String,
+  birthday: String,
+  aboutMe: String,
+  followerList: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      followedOn: { type: Date, default: Date.now },
+    },
+  ],
+  skills: [{ type: String }],
 });
 
-  
+// userSkills is a virtual field
+userSchema.virtual('userSkills').get(function () {
+  return data.sportsSkills
+    .filter((x) => this.skills.indexOf(x.value) > -1)
+    .map((y) => y.text);
+});
+userSchema.set('toObject', { virtuals: true });
+userSchema.set('toJSON', { virtuals: true });
 
+
+
+const User = mongoose.model('User', userSchema);
 module.exports = User;
